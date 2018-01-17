2e8e8e36c15234b74e4a61fc8c5cb2cfed680f55
Murilo Fujita <murilofujita@fujita.com>
2018-01-17 16:14:56 -0200

    Frutas dispostas em um grid.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour 
+{
+	int gridSizeX = 6, gridSizeY = 6;
+
+	private GameObject[] _fruits;
+	private GridItem[,] _items;
+
+	void Start () 
+	{
+		GetFruits ();
+		CreateGrid ();
+	}	
+
+	void CreateGrid () 
+	{
+		_items = new GridItem[gridSizeX, gridSizeY];
+
+		for (int x = 0; x < gridSizeX; x++) 
+		{
+			for (int y = 0; y < gridSizeY; y++) 
+			{
+				_items [x, y] = InstantiateFruit (x,y);
+
+			}	
+		}
+	}
+
+	GridItem InstantiateFruit(int xPos, int yPos)
+	{
+		print (xPos + " " + yPos);
+		GameObject ramdomFruit = _fruits [UnityEngine.Random.Range (0, _fruits.Length)];
+		GridItem newFruit = ((GameObject)Instantiate (ramdomFruit, new Vector3 (xPos * 1.6f-2, yPos*1.6f-4), Quaternion.identity)).GetComponent<GridItem> ();
+
+		newFruit.OnItemPositionChanged (xPos, yPos);
+
+		return newFruit;
+	}
+
+	void GetFruits()
+	{
+		_fruits = Resources.LoadAll<GameObject> ("Frutas");
+	}
+}
---
+++ Assets/Scripts/GridItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridItem : MonoBehaviour 
+{
+	public int x {
+		get;
+		private set;
+	}
+
+	public int y {
+		get;
+		private set;
+	}
+
+	public void OnItemPositionChanged(int newX, int newY)
+	{
+		x = newX;
+		y = newY;
+
+		gameObject.name = string.Format ("Sprite " + x + y);
+	}
+
+	private void OnMouseDow()
+	{
+		if (OnMouseOverItemEventHandler != null) 
+		{
+			OnMouseOverItemEventHandler (this);
+		}
+	}
+
+	public delegate void OnMouseOverItem(GridItem item);
+	public static event OnMouseOverItem OnMouseOverItemEventHandler;
+}
